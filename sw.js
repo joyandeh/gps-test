eeb9444a1f52e0f3d9d1ff4f9e87d2af51517196
Erfan jooyandeh <erfan.joyandeh@gmail.com>
2025-12-06 22:15:14 +0330

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'praytimes-cache-v1';
+const urlsToCache = [
+    '/gps-test/',
+    '/gps-test/index.html',
+    '/gps-test/css/style.css',
+    '/gps-test/js/app.js',
+    '/gps-test/js/cities.js',
+    '/gps-test/js/praytime.js',
+    '/gps-test/manifest.json',
+    '/gps-test/cities.csv' 
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => cache.addAll(urlsToCache))
+        .then(() => self.skipWaiting()) // فوراً SW جدید فعال شود
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim()); // کنترل همه تب‌ها را بگیرد
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+        .then(response => response || fetch(event.request))
+    );
+});
